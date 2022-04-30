d603f22de78152073f57641115d861d5e889fef2
miura19 <littlecloud194@gmail.com>
2022-05-01 01:03:42 +0900

    ★チャットルームの作成★

--- app/User.php
+++ app/User.php
@@ -45,4 +45,13 @@ class User extends Authenticatable
         return $this->hasMany('App\Reaction','from_user_id','id');
     }
     
+    public function chatMessages()
+    {
+        return $this->hasMany('App\ChatMessage');
+    }
+
+    public function chatRoomUsers()
+    {
+        return $this->hasMany('App\ChatRoomUsers');
+    }
 }
